b88979b9a47d75884c36b4c20eb1cdbb2134067b
saeedark5-wq <saeedark5-wq@github.com>
2026-06-11 19:29:11 +0400

    Use img tag for ARK logo for better compatibility

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -129,7 +129,7 @@ export default function Footer() {
       <div className="relative border-t border-white/10 py-6">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col sm:flex-row items-center justify-between gap-4 text-sm text-white/40">
           <p>&copy; {new Date().getFullYear()} {COMPANY_NAME}. All rights reserved.</p>
-          <p className="flex items-center gap-1">Designed &amp; Developed by <Image src="/images/ark.jfif" alt="ARK" width={20} height={20} className="w-5 h-5 inline-block rounded-full" unoptimized /> Muhammad Saeed</p>
+          <p className="flex items-center gap-1">Designed &amp; Developed by <img src="/images/ark.jfif" alt="ARK" width="20" height="20" style={{ borderRadius: "50%", width: "20px", height: "20px", objectFit: "cover", display: "inline-block" }} /> Muhammad Saeed</p>
         </div>
       </div>
     </footer>
